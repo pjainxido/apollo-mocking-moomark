2688e9a3d917b1bffddbd9c931bea319173e8627
Park <pjane123@naver.com>
2021-07-14 00:22:19 +0900

    mocking 데이터 추가

--- index.js
+++ index.js
@@ -1,20 +1,78 @@
-const { ApolloServer, gql } = require("apollo-server");
+import { PostData } from './data.js';
+import { ApolloServer, gql, makeExecutableSchema, MockList } from 'apollo-server';
+import { GraphQLScalarType } from 'graphql';
+import faker from 'faker';
+
+const title = ['test', '마크타운데 관하여', 'about react', 'apollo'];
+const author = []
+
+// const dateScalar = new GraphQLScalarType({
+//   name: 'Date',
+//   parseValue(value) {
+//     return new Date(value);
+//   },
+//   serialize(value) {
+//     return value.toISOString();
+//   },
+// });
+
+const mocks = {
+  Id: () => faker.datatype.uuid(),
+  Int: () => faker.datatype.number(100),
+  String: () => faker.name.firstName(),
+  Date: () => faker.datatype.datetime(),
+  PostListData: () => ({
+    title: () => faker.random.arrayElement(title),
+    author: () => faker.name.firstName(),
+  }),
+  Query: () => ({
+    ping: () => 'pong',
+    postlist: () => new MockList(100),
+  }),
+};
 
 const typeDefs = gql`
+  scalar Date
   type Query {
     ping: String
+    postData: PostData!
+    postlist: [PostListData]!
+  }
+  type PostData {
+    id: ID!
+    title: String!
+    postDate: Date!
+    editorName: String!
+    content: String!
+    tags: [String!]!
+    description: String!
+    like: Int!
+    thumbNail: String
+  }
+  type PostListData {
+    id: ID!
+    title: String
+    author: String
+    postDate: Date
+    tags: [String]
+    description: String
+    like: Int
+    thumbNail: String
   }
 `;
 
-const resolvers = {
-  Query: {
-    ping: () => "pong",
-  },
-};
+// const resolvers = {
+//   Query: () => ({
+//     ping: () => 'pong',
+//     postlist: () => new MockList(10),
+//   }),
+// };
 
 const server = new ApolloServer({
+  // resolvers,
   typeDefs,
-  resolvers,
+  mocks,
+  mockEntireSchema: false,
 });
 
 server.listen(4004).then(({ url }) => {
